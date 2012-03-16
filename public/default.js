94795ee46a5ce322aa4d5520a0d92061889436d6
Ryan Chan <ryan@rcchan.com>
2012-03-16 07:19:23 -0400

    Default text for title and message fields

--- public/default.js
+++ public/default.js
@@ -1,13 +1,19 @@
-$(window).load(
-  function(){
-    $('.publisher .pheedto').focus(
+function defaultText(e, text){
+  $(e).focus(
       function(){
         $(this).val('').removeClass('greyed');
       }
     ).blur(
       function(){
-        if (!$(this).val()) $(this).val('pheed.it user or email(s)').addClass('greyed');
+        if (!$(this).val()) $(this).val(text).addClass('greyed');
       }
     );
+}
+
+$(window).load(
+  function(){
+    defaultText('.publisher .title', 'Title your post here');
+    defaultText('.publisher .message', 'Enter your text here');
+    defaultText('.publisher .pheedto', 'pheed.it user or email(s)');
   }
 );
