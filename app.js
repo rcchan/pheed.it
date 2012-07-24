1b2827c27c0d5e63727855895572c3a8a74633eb
Ryan Chan <ryan@rcchan.com>
2012-07-24 03:57:07 -0400

    Basic user schema

--- app.js
+++ app.js
@@ -120,6 +120,14 @@ var PostSchema = new Schema({
 PostSchema.index({location: '2d'});
 Post = mongoose.model('Post', PostSchema);
 
+var UserSchema = new Schema({
+  username: {type: String, index: true, required: true},
+  password: {type: String, required: true},
+  first_name: String,
+  last_name: String,
+  email: String
+});
+User = mongoose.model('User', UserSchema);
 // Helpers
 
 htmlentities = function(str) {
