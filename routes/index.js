7e11a903d688a6cb0fc02b6abaa96948d421ec58
Ryan Chan <ryan@rcchan.com>
2012-07-26 00:39:10 -0400

    Show login / logged in user

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'pheed.it' });
+  res.render('index', { title: 'pheed.it', user: req.user });
 };
 
 exports.login = function(req, res){
