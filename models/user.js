74d344845824fca8346c6f5781a3c977fff46eba
Ryan Chan <ryan@rcchan.com>
2012-09-28 00:51:01 -0400

    Require unique usernames

--- models/user.js
+++ models/user.js
@@ -2,12 +2,13 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema, ObjectId = Schema.ObjectId;
 
 var UserSchema = new Schema({
-  username: {type: String, index: true, required: true},
-  password: {type: String, required: true},
+  username: {type: String, index: {unique: true}, required: true, trim: true},
+  password: {type: String, required: true, trim: true},
   first_name: String,
   last_name: String,
   email: String,
   fbid: String,
   homepage: Schema.Types.Mixed
 });
+
 module.exports = mongoose.model('User', UserSchema);
