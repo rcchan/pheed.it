e4b1705a60b2544975da6423cca2ae91f483fad7
Ryan Chan <ryan@rcchan.com>
2012-09-28 02:06:56 -0400

    Submit login form properly

--- assets/js/splash.js
+++ assets/js/splash.js
@@ -3,10 +3,13 @@ pheedit.splash = {
   init: function(){
     $(window).load(function(){
       $('.button.signup').click(function(){
-        return !$(this).parentsUntil('form').parent().prop('action', '/sign_up').ajaxSubmit(function(r, s, x, f){
-          if (r.username) $(f).prop('action', '/login').submit();
+        var $f = $(this).parentsUntil('form').parent().prop('action', '/sign_up')
+        $f.ajaxSubmit(function(r){
+          if (r.username) $f.prop('action', '/login').submit();
           else alert(r.err);
-        });
+        })
+        $f.prop('action', '/login');
+        return false;
       });
     });
   }
