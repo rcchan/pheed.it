77b7deb648e981c41e4e67fb9b20de92f24fc3f5
Ryan Chan <ryan@rcchan.com>
2012-10-20 03:51:28 -0400

    Move files for mincer

---
+++ assets/javascripts/index.js
@@ -0,0 +1,6 @@
+//= require_self
+//= require_directory .
+//= require_directory fancybox
+
+var pheedit = pheedit || {};
+pheedit.CDN_HOST = (function(h){a=document.createElement('a');a.href=h;return a.hostname})(document.location.href);
--- assets/js/index.js
+++
@@ -1,6 +0,0 @@
-var pheedit = pheedit || {};
-pheedit.CDN_HOST = (function(h){a=document.createElement('a');a.href=h;return a.hostname})(document.location.href);
-
-//= require_self
-//= require_tree .
-//= require fancybox
